cab7dd4b768a2ba9db3a05c4a42f30219bb7e46e
Vinu <vinu@cs.utah.edu>
2019-08-26 14:18:16 -0600

    Update functional.py

--- python/functional.py
+++ python/functional.py
@@ -1,1 +1,24 @@
-#test
+# functional programming how to
+# ideas from https://docs.python.org/3.5/howto/functional.html#functional-howto-iterators
+
+# introduction
+
+# formal provability
+
+# modularity
+
+# easy of debugging and testing
+
+# composability
+
+# iterators
+
+L = [1,2,3]
+it = iter(L)
+it
+it.__next__
+next(it)
+
+# data types that support iterators
+# generator expressions and list comprehensions
+# generators
